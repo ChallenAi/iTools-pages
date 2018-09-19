8cbf6ce2c2faa1879bacf506cb75355f27a96ac8
ChallenAi <jianglun465@sina.com>
2018-09-20 01:06:50 +0800

    add time add request support

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import format from 'date-fns/format';
 
 class App extends Component {
   render() {
@@ -7,6 +8,11 @@ class App extends Component {
       <div className="App">
         <p className="App-intro">
           hello, pages
+        </p>
+        <p className="App-intro">
+          {
+            format(new Date(), 'YYYY MMMM do, h:mm:ss')
+          }
         </p>
       </div>
     );
